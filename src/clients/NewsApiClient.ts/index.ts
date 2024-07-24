153bcc46f8a7a031ca7e739321cb849ce9971652
Bruno Fernando dos Santos Silva <bruno2fernando@gmail.com>
2024-07-24 01:06:19 -0300

    chore: remove unused method

--- src/clients/NewsApiClient.ts/index.ts
+++
@@ -1,9 +0,0 @@
-import axios, { AxiosInstance } from "axios";
-
-const baseURL = "https://api.my-domain.com/";
-
-const newsPostApi: AxiosInstance = axios.create({
-  baseURL,
-});
-
-export default newsPostApi;
